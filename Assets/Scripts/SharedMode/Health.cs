3aa05cb4cef87d34885100a8575c63d09616a3ce
youngil <youngil@placeb.com>
2024-02-02 11:21:44 +0900

    Rpc

---
+++ Assets/Scripts/SharedMode/Health.cs
@@ -0,0 +1,24 @@
+using Fusion;
+using UnityEngine;
+
+namespace SharedMode
+{
+    public class Health : NetworkBehaviour
+    {
+        [Networked, OnChangedRender(nameof(HealthChanged))]
+        public float NetworkedHealth { get; set; } = 100;
+
+        void HealthChanged()
+        {
+            Debug.Log($"Health changed to: {NetworkedHealth}");
+        }
+
+        [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
+        public void DealDamageRpc(float damage)
+        {
+            Debug.Log($"Received DealDamageRpc on StateAuthority, modifying Networked variable");
+            NetworkedHealth -= damage;
+        }
+    }
+
+}
